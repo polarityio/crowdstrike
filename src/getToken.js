99770dc0a11503def95fddd907fabc13e6561c80
ntsmith7 <nick@polarity.io>
2022-02-25 12:36:50 -0800

    get token function

--- src/getToken.js
+++ src/getToken.js
@@ -20,32 +20,26 @@ const generateAccessToken = async (requestWithDefaults, options, Logger) => {
 
     const { body } = response;
 
-    Logger.trace({ TOKEN_RESPONSE: response });
-
     if (response.statusCode === 201 && body.access_token) {
-      Logger.trace({ CACHE: response });
       setTokenInCache(options, body.access_token, Logger);
-      Logger.trace({ CACHE: tokenCache });
       return response.body.access_token;
     }
   } catch (err) {
     Logger.trace({ err }, 'err in generating tokens');
+    throw err;
   }
 };
 
-const getTokenFromCache = (options, Logger) => {
-  return tokenCache.get(_getTokenKey(options, Logger));
+const getTokenFromCache = (options) => {
+  return tokenCache.get(_getTokenKey(options));
 };
 
-const setTokenInCache = (options, token, Logger) => {
-  tokenCache.set(_getTokenKey(options, Logger), token);
+const setTokenInCache = (options, token) => {
+  tokenCache.set(_getTokenKey(options), token);
 };
 
-const _getTokenKey = (options, Logger) => {
+const _getTokenKey = (options) => {
   return options.url + options.id + options.secret;
 };
-//create a new token,
-// invalid token
-// expired token
 
 module.exports = generateAccessToken;
