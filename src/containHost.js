25f38de8eb9a7a3a2ca000c64acfa9249cd09e76
ntsmith7 <nick@polarity.io>
2022-02-25 12:38:12 -0800

    contain host function

--- src/containHost.js
+++ src/containHost.js
@@ -1,13 +1,29 @@
-const containHost = async (requestWithDefaults, token, entity, options, Logger) => {
-  //will probably need
-  const requestOptions = {
-    method: 'POST',
-    uri: `${options.url}/devices/entities/devices-actions/v2`,
-    body: {
-      ids: []
+const containHost = async (authenticatedRequest, requestWithDefaults, data, options, Logger) => {
+  const status = data.status;
+  const deviceId = data.id;
+
+  try {
+    if (status === 'normal' || status === 'contained') {
+      const requestOptions = {
+        method: 'POST',
+        uri: `${options.url}/devices/entities/devices-actions/v2?action_name=${
+          status === 'normal'
+            ? 'contain'
+            : 'lift_containment' || status === 'contained'
+            ? 'lift_containment'
+            : 'contained'
+        }`,
+        body: { ids: [deviceId] },
+        json: true
+      };
+
+      Logger.trace({ requestOptions }, 'containHost requestOptions');
+      const response = await authenticatedRequest(requestWithDefaults, requestOptions, options, Logger);
+      return response;
     }
-  };
-  const response = await requestWithDefaults(requestOptions);
+  } catch (err) {
+    throw err;
+  }
 };
 
-module.exports = containHost;
+module.exports = { containHost };
